103502112489e77d714e1db4addc56ca23be4cf1
Maciej Szumowski <mpszumowski@gmail.com>
2017-12-21 13:52:02 +0100

    add update if country exists function to scrapers

--- scrapers/scrapers/pipelines.py
+++ scrapers/scrapers/pipelines.py
@@ -22,8 +22,12 @@ class CountryPipeline(object):
     def close_spider(self, spider):
         country_bulk = []
         for item in self.items:
-            obj = Countries(name=item["name"][0])
-            country_bulk.append(obj)
+            country = Countries.objects.filter(name__iexact=item["name"][0])
+            if country:
+                pass
+            else:
+                obj = Countries(name=item["name"][0])
+                country_bulk.append(obj)
         Countries.objects.bulk_create(country_bulk)
 
 
@@ -41,11 +45,18 @@ class PopulationPipeline(object):
         for item in self.items:
             country = Countries.objects.filter(name__iexact=item["country"][0])
             if country:
-                obj = CountryPopulation(country=country[0],
-                                        estimate=item["estimate"][0],
-                                        data_year=item["data_year"][0],
-                                        date_scraped=datetime.now())
-                population_bulk.append(obj)
+                c_population_object = CountryPopulation.objects.filter(
+                    country=country[0])
+                if c_population_object:
+                    c_population_object.update(estimate=item["estimate"][0],
+                                               data_year=item["data_year"][0],
+                                               date_scraped=datetime.now())
+                else:
+                    obj = CountryPopulation(country=country[0],
+                                            estimate=item["estimate"][0],
+                                            data_year=item["data_year"][0],
+                                            date_scraped=datetime.now())
+                    population_bulk.append(obj)
             else:
                 obj = Misfits(name=item["country"][0],
                               population_estimate=item["estimate"][0],
@@ -70,10 +81,15 @@ class FoodPipeline(object):
         for item in self.items:
             country = Countries.objects.filter(name__iexact=item["country"][0])
             if country:
-                obj = MinimumAmount(country=country[0],
-                                    amount=item["amount"][0],
-                                    date_scraped=datetime.now())
-                min_amount_bulk.append(obj)
+                c_food_object = MinimumAmount.objects.filter(country=country[0])
+                if c_food_object:
+                    c_food_object.update(amount=item["amount"][0],
+                                         date_scraped=datetime.now())
+                else:
+                    obj = MinimumAmount(country=country[0],
+                                        amount=item["amount"][0],
+                                        date_scraped=datetime.now())
+                    min_amount_bulk.append(obj)
             else:
                 obj = Misfits(name=item["country"][0],
                               minimum_amount=item["amount"][0],
@@ -97,11 +113,18 @@ class PovertyPipeline(object):
         for item in self.items:
             country = Countries.objects.filter(name__iexact=item["country"][0])
             if country:
-                obj = PovertyPercent(country=country[0],
-                                     percent=item["percent"][0],
-                                     data_year=item["data_year"][0],
-                                     date_scraped=datetime.now())
-                poverty_bulk.append(obj)
+                c_poverty_object = PovertyPercent.objects.filter(
+                    country=country[0])
+                if c_poverty_object:
+                    c_poverty_object.update(percent=item["percent"][0],
+                                            data_year=item["data_year"][0],
+                                            date_scraped=datetime.now())
+                else:
+                    obj = PovertyPercent(country=country[0],
+                                         percent=item["percent"][0],
+                                         data_year=item["data_year"][0],
+                                         date_scraped=datetime.now())
+                    poverty_bulk.append(obj)
             else:
                 obj = Misfits(name=item["country"][0],
                               percent=item["percent"][0],
