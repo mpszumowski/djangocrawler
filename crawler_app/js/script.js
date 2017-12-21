6c58cd2ed21ef1eea8c2f0b3ce33c4c89bb39886
Maciej Szumowski <mpszumowski@gmail.com>
2017-12-21 11:30:01 +0100

    random country draw function

--- crawler_app/js/script.js
+++ crawler_app/js/script.js
@@ -1,24 +1,74 @@
 $(document).ready(function() {
 
-
     $.ajax({
         url: 'http://127.0.0.1:8000/countries/'
     }).done(function(data) {
-        var main = $("main");
+        var tbody = $("tbody");
 
         for (var i = 0; i < data.length; i++) {
+            var name = data[i].name;
+            // limit data to countries and World
+            if (name == "East Asia & Pacific") {
+                break
+            }
+            // clean and parse population data
+            var population = data[i].population[0].estimate;
+            if (population == "0.0") {
+                var population = "0.01";
+            }
+            if (population == "..") {
+                var population = "5.0"
+            }
+            var population = population.replace(",", "")
+            // clean poverty data
+            if (data[i].poverty.length != 0) {
+                var poverty = data[i].poverty[0].percent
+            } else {
+                var poverty = "brak danych"
+            }
+            // clean amount data
+            if (data[i].amount.length != 0) {
+                var amount = data[i].amount[0].amount;
+                if (amount == "?") {
+                    var amount = "dane niepełne"
+                }
+            } else {
+                var amount = "brak danych"
+            }
             var countryInfo = $(`
-                <tr>
-                    <td>${data[i].name}</td>
-                    <td>${data[i].population[0].estimate}</td>
-                    <td>${data[i].poverty[0].percent}</td>
-                    <td>${data[i].amount[0]}</td>
+                <tr data-region="${name}">
+                    <td>${name}</td>
+                    <td>${population}</td>
+                    <td>${poverty}</td>
+                    <td>${amount}</td>
                 </tr>
             `);
-            main.append(countryInfo);
+            tbody.append(countryInfo);
         }
+        // function drawing from random country in the world
+        var random = function(upperLimit) {
+            return Math.random() * upperLimit
+        };
+        // enter lists from name column, pop column and total world population
+        var getRandomCountry = function(list, weight, max) {
+            var max = parseFloat($(max).text());
+            var sample = random(max);
+            var total = 0;
+            for (var i = 0; i < list.length; i++) {
+                var total = parseFloat(weight[i].innerText) + total;
+                if (total > sample) {
+                    return list[i].innerText
+                }
+            }
+            var randomNum = random(total)
+        };
+        var worldTotal = $('[data-region="World"] > td')[1];
+        var regionList = $("td:first-child");
+        var populationList = $("td:nth-child(2)");
+        console.log(getRandomCountry(regionList, populationList, worldTotal))
     });
 });
+
 
 
 
