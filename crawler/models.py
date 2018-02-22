2ac53d526207a10f8665bb0d29aba255c9d12bed
Maciej Szumowski <mpszumowski@gmail.com>
2018-02-22 15:52:47 +0100

    add mixins with country names and date scraped

--- crawler/models.py
+++ crawler/models.py
@@ -13,27 +13,40 @@ class Countries(models.Model):
         return countries
 
 
-class MinimumAmount(models.Model):
+class CountryMixin(models.Model):
+    """Links each model containing data with the list of countries"""
     country = models.ForeignKey(Countries, on_delete=True)
-    amount = models.CharField(max_length=16)
+
+    class Meta:
+        abstract = True
+
+
+class DateScrapedMixin(models.Model):
+    """Adds info about the date of scraping the data"""
     date_scraped = models.DateField()
 
+    class Meta:
+        abstract = True
 
-class PovertyPercent(models.Model):
-    country = models.ForeignKey(Countries, on_delete=True)
+
+class MinimumAmount(CountryMixin,
+                    DateScrapedMixin):
+    amount = models.CharField(max_length=16)
+
+
+class PovertyPercent(CountryMixin,
+                     DateScrapedMixin):
     percent = models.CharField(max_length=16)
     data_year = models.CharField(max_length=16, null=True)
-    date_scraped = models.DateField()
 
 
-class CountryPopulation(models.Model):
-    country = models.ForeignKey(Countries, on_delete=True)
+class CountryPopulation(CountryMixin,
+                        DateScrapedMixin):
     estimate = models.CharField(max_length=32)
     data_year = models.CharField(max_length=16, null=True)
-    date_scraped = models.DateField()
 
 
-class Misfits(models.Model):
+class Misfits(DateScrapedMixin):
     name = models.CharField(max_length=64)
     minimum_amount = models.CharField(max_length=32, null=True)
     minimum_amount_year = models.CharField(max_length=16, null=True)
@@ -41,4 +54,3 @@ class Misfits(models.Model):
     percent_year = models.CharField(max_length=16, null=True)
     population_estimate = models.CharField(max_length=32, null=True)
     population_year = models.CharField(max_length=16, null=True)
-    date_scraped = models.DateField()
